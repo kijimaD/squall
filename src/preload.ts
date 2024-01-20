cbace9b1171dcf34d8a8b9bd5bc3dfaee491bfac
Kijima Daigo <norimaking777@gmail.com>
2024-01-20 15:30:30 +0900

    build: npm init electron-app@latest squall -- --template=webpack-typescript

--- src/preload.ts
+++ src/preload.ts
@@ -1,1 +1,2 @@
-console.log("preloaded!");
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
