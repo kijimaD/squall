a5db767621eb08d9792135ae8a7db72bd85ee358
Kijima Daigo <norimaking777@gmail.com>
2024-01-21 19:47:26 +0900

    style: run format

--- front/src/renderer/App.tsx
+++ front/src/renderer/App.tsx
@@ -1,54 +1,69 @@
-import CssBaseline from '@mui/material/CssBaseline'
+import CssBaseline from "@mui/material/CssBaseline";
 import { Container, Grid, Paper, Button, Typography } from "@mui/material";
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-import TornadoIcon from '@mui/icons-material/Tornado';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import TornadoIcon from "@mui/icons-material/Tornado";
 import "./App.css";
 
 const theme = createTheme({
-    typography: {
-        button: {
-            textTransform: "none"
-        },
+  typography: {
+    button: {
+      textTransform: "none",
     },
-    palette: {
-        black: {
-            main: '#333333',
-        },
+  },
+  palette: {
+    black: {
+      main: "#333333",
     },
-})
+  },
+});
 
 export const App = () => {
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Typography variant="h3"><TornadoIcon sx={{ color: 'blue', fontSize: 34 }} />Squall</Typography>
-            <Container maxWidth="lg" sx={{ mt: 2, mb: 2 }} className="container">
-                <Grid container direction="row" spacing={2}>
-                    <Grid item xs={12} sm={6} spacing={1}>
-                        <Grid container direction="column">
-                            <Button color="black"
-                                    style={{justifyContent: "flex-start"}}
-                                    onClick={() => {
-                                        window.myAPI.invoke("changeTab", {url: "main_window"});
-                                    }}>Home</Button>
-                        </Grid>
-                        <Grid container direction="column">
-                            <Button color="black"
-                                    style={{justifyContent: "flex-start"}}
-                                    onClick={() => {
-                                        window.myAPI.invoke("changeTab", {url: "google.com"});
-                                    }}>Google</Button>
-                        </Grid>
-                        <Grid container direction="column">
-                            <Button color="black"
-                                    style={{justifyContent: "flex-start"}}
-                                    onClick={() => {
-                                        window.myAPI.invoke("changeTab", {url: "amazon.com"});
-                                    }}>Amazon</Button>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Container>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Typography variant="h3">
+        <TornadoIcon sx={{ color: "blue", fontSize: 34 }} />
+        Squall
+      </Typography>
+      <Container maxWidth="lg" sx={{ mt: 2, mb: 2 }} className="container">
+        <Grid container direction="row" spacing={2}>
+          <Grid item xs={12} sm={6} spacing={1}>
+            <Grid container direction="column">
+              <Button
+                color="black"
+                style={{ justifyContent: "flex-start" }}
+                onClick={() => {
+                  window.myAPI.invoke("changeTab", { url: "main_window" });
+                }}
+              >
+                Home
+              </Button>
+            </Grid>
+            <Grid container direction="column">
+              <Button
+                color="black"
+                style={{ justifyContent: "flex-start" }}
+                onClick={() => {
+                  window.myAPI.invoke("changeTab", { url: "google.com" });
+                }}
+              >
+                Google
+              </Button>
+            </Grid>
+            <Grid container direction="column">
+              <Button
+                color="black"
+                style={{ justifyContent: "flex-start" }}
+                onClick={() => {
+                  window.myAPI.invoke("changeTab", { url: "amazon.com" });
+                }}
+              >
+                Amazon
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </ThemeProvider>
+  );
 };
