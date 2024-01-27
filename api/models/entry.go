1656d399b52529b2df904d176dfad5ffbe0a212a
Kijima Daigo <norimaking777@gmail.com>
2024-01-27 10:29:52 +0900

    feat: 制約を追加した

--- api/models/entry.go
+++ api/models/entry.go
@@ -1,7 +1,10 @@
 package models
 
+import "gorm.io/gorm"
+
 type Entry struct {
+	gorm.Model
 	ID     *uint  `gorm:"primaryKey,column:id" json:"id"`
-	URL    string `gorm:"column:url;not null" json:"url"`
+	URL    string `gorm:"column:url;not null;unique" json:"url"`
 	IsDone bool   `gorm:"column:is_done;default:false;not null" json:"is_done"`
 }
